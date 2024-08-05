353b4a31f08a73ef8ddef8934c76c2f287e1b6f9
Himanshipatel04 <himanshipatel0409@gmail.com>
2024-08-05 16:02:33 +0530

    chore: Add django_browser_reload app to project

--- newproject/newproject/settings.py
+++ newproject/newproject/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'chai',
     'tailwind',
-    'theme'
+    'theme',
+    'django_browser_reload',
 ]
 
 TAILWIND_APP_NAME = 'theme'
@@ -54,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware'
 ]
 
 ROOT_URLCONF = 'newproject.urls'
